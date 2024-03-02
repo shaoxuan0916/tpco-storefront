3a73445aa1b025bb8eeb99d300973ac5dd522ff5
shaoxuan <shaoxuanhinhua@gmail.com>
2024-03-03 02:35:12 +0800

    first commit

---
+++ next.config.js
@@ -0,0 +1,26 @@
+const { withStoreConfig } = require("./store-config")
+const store = require("./store.config.json")
+
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = withStoreConfig({
+  features: store.features,
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+      },
+      {
+        protocol: "https",
+        hostname: "gkmmhlnurcujttiothvi.supabase.co",
+      },
+    ],
+  },
+})
+
+console.log("next.config.js", JSON.stringify(module.exports, null, 2))
+
+module.exports = nextConfig
